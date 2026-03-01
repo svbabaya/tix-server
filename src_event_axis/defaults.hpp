4c3cba492866a9c5aa5f06e8661aeae98f96b050
svbabaya <babayan.serge@gmail.com>
2026-03-01 15:35:25 +0300

    Fix defaults

--- src_event_axis/defaults.hpp
+++ src_event_axis/defaults.hpp
@@ -9,20 +9,20 @@ const std::string CAPTURE_FORMAT = "Y800";
 const long BYTES_IN_MB = 1024 * 1024;
 const int MAX_STATISTICS_FILE_SIZE_MB = 5;
 
-/*** From legacy _common.h */ 
-/* #define GLOBAL_UPDATE_STAT_FRAMENUM		1  //интервал (кадры) обновления глобальной статистики
-/* #define GLOBAL_UPDATE_CONFIG_FRAMENUM	10 //проверки конфигурации
-/*** */
+/**** 
+//// From legacy _common.h
+#define GLOBAL_UPDATE_STAT_FRAMENUM		1  //интервал (кадры) обновления глобальной статистики
+#define GLOBAL_UPDATE_CONFIG_FRAMENUM	10 //проверки конфигурации
 
-/*** From legacy common/_common.h */ 
-/* #define COMPENSATE_LIGHTCHANGE 1 //Включить компенсацию быстрого изменения яркости
-/*
-/*	#define CONNECT_TO_CAMERA_TCP_SERVER_TIMEOUT_MS		10000
-/*	#define CONNECT_TO_CAMERA_COMMON_TIMEOUT_MS			3000
-/*	#define CONNECT_TO_CAMERA_VIDEO_TIMEOUT_MS			50
-/*	#define CONNECT_TO_CAMERA_VIDEO_TIMEOUT_NUM			300 //all time=CONNECT_TO_CAMERA_VIDEO_TIMEOUT_NUM*CONNECT_TO_CAMERA_VIDEO_TIMEOUT_MS
-/*
-/*	//tcpclient
-/*	#define COMMAND_DELAY_MS	100
-/*	#define CHAR_BUFFER_SIZE	8192
-/*** */
+//// From legacy common/_common.h
+#define COMPENSATE_LIGHTCHANGE 1 //Включить компенсацию быстрого изменения яркости
+
+#define CONNECT_TO_CAMERA_TCP_SERVER_TIMEOUT_MS		10000
+#define CONNECT_TO_CAMERA_COMMON_TIMEOUT_MS			3000
+#define CONNECT_TO_CAMERA_VIDEO_TIMEOUT_MS			50
+#define CONNECT_TO_CAMERA_VIDEO_TIMEOUT_NUM			300 //all time=CONNECT_TO_CAMERA_VIDEO_TIMEOUT_NUM*CONNECT_TO_CAMERA_VIDEO_TIMEOUT_MS
+
+//tcpclient
+#define COMMAND_DELAY_MS	100
+#define CHAR_BUFFER_SIZE	8192
+****/
